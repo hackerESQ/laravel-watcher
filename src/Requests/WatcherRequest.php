4c0bfa41d4b694253549f9fa7bc9047056df91f2
hackerESQ <corey@coreyvarma.com>
2021-02-05 19:06:33 -0600

    no need for a traits directory

--- src/Requests/WatcherRequest.php
+++ src/Requests/WatcherRequest.php
@@ -2,12 +2,21 @@
 
 namespace hackerESQ\Watcher\Requests;
 
-use hackerESQ\Watcher\Traits\Watcher;
+use hackerESQ\Watcher\Watcher;
 use Illuminate\Foundation\Http\FormRequest;
 
 class WatcherRequest extends FormRequest
 {
     use Watcher;
+
+    /**
+     * This is a base FormRequest that allows you to quickly use Watcher. 
+     * To use this, replace (Request $request) with (WatcherRequest $request)
+     * in your controller.
+     * 
+     * Alternatively, you can create your own custom FormRequest and use the 
+     * Watcher trait that comes with this package.
+     */
     
     /**
      * Determine if the user is authorized to make this request.
