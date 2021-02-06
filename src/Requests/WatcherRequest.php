6dd79c3586ad0beb2ce59c7ea274aa02f5eb612c
hackerESQ <corey@coreyvarma.com>
2021-02-05 19:02:43 -0600

    initial

---
+++ src/Requests/WatcherRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace hackerESQ\Watcher\Requests;
+
+use hackerESQ\Watcher\Traits\Watcher;
+use Illuminate\Foundation\Http\FormRequest;
+
+class WatcherRequest extends FormRequest
+{
+    use Watcher;
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
---
+++ src/Traits/Watcher.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace hackerESQ\Watcher\Traits;
+
+/**
+ * INSTALL:
+ * 
+ * 1. Create custom FormRequest and "use" this trait in your custom FormRequest
+ * 2. Use the default WatcherRequest if you don't need your own FormRequest
+ * 3. Add your watched triggers with $request->setWatcher()
+ * 
+ * USAGE:
+ * 
+ * $request->setWatcher([
+ *      'invoice_start_num_changed' => [
+ *          'action' => fn($context) => DB::statement("ALTER TABLE `invoices` AUTO_INCREMENT = ".(int)$context->request->invoice_start_num),
+ *          'removeKey' => true,
+ *      ],
+ *  ]);
+ * 
+ */ 
+
+trait Watcher
+{
+    private array $triggers = [];
+    private array $keys_to_remove = [];
+
+    /**
+     * Sets the triggers for the watcher.
+     *
+     * @param array $triggers The keys to watch and the actions to perform if present in request
+     * @return void
+     */
+    public function setWatcher($triggers) {
+        $this->triggers = $triggers;
+
+        $this->watch();
+    }
+
+    /**
+     * Watches for specific keys and performs the defined action.
+     *
+     * @return void
+     */
+    private function watch() {
+        // loop through triggers
+        foreach($this->triggers as $trigger => $meta) {
+            // does this trigger exist in the request?
+            if ( $this->has($trigger) && !empty($this->{$trigger}) ) {
+                // run action and provide context to function as an object
+                $meta['action']((object)[
+                    'request' => $this,
+                    'trigger' => $trigger
+                ]);
+
+                // should we remove this key?
+                if (isset($meta['removeKey']) && $meta['removeKey']) array_push($this->keys_to_remove,$trigger);
+            }
+        }
+        // are there any keys to remove? if so, do it...
+        if ($this->keys_to_remove) $this->replace($this->except($this->keys_to_remove));
+    }
+}
